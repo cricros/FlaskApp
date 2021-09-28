7fa95e29ad558b6236f5d0b9e90d53b6185261c3
Cristian <vce11987@innovaccion.mx>
2021-09-27 22:56:46 -0500

    Hola

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+E
+static/css/styles.css,f/f/ff5f6247d17a4d94e3cf2aed570cdefe538f01e5
+=
+cursoflask.py,8/a/8a83ca8f44017cc21a040193e7695155411d9957
+K
+templates/select_table.html,8/e/8e3ab5b089ce694a0ccabcc270c59c97234ce4bf
+6
+app.py,1/3/13cce7fd076299c81b4986166f3d822791c9490e
+D
+static/js/scripts.js,4/d/4dc6053ee96817d0caa0ea943ea6d475fa3593b2
+I
+templates/simpleform.html,5/8/5853cd071a549c3e115640ba68c02abb4cb52c49
+D
+templates/index.html,f/6/f69b8ce2e31684ae06f1b3e84821e07e3dfddb2f
---
+++ pruebasbd.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template,request
+from flask_mysqldb import MySQL
+'''
+Si estas usando python y quieres usar la flask_mysqldb recuerda instalar primero: 
+1) sudo apt-get install python-dev default-libmysqlclient-dev libssl-dev y al final
+2) pip install --user flask-mysqldb 
+
+
+#iniciar xampp sudo /opt/lampp/manager-linux-x64.run
+
+'''
+app = Flask(__name__)
+#Conexion BD
+
+
+#si no te permite conectar con localhost intenta con 127.0.0.1.
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'pruebas_python'
+
+mysql = MySQL(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html") #cambiar el formato de index.hmtl
+
+
+
+@app.route('/comp1', methods=['GET','POST'])
+def comp1():
+
+    cursor = mysql.connection.cursor()
+
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+
+        cursor.execute("USE pruebas_python")
+        cursor.execute("INSERT into pruebas (name,email) VALUES (%s,%s)",(name,email))
+
+        mysql.connection.commit()
+
+        cursor.close()
+
+        return render_template("simpleform.html")
+
+    return render_template('simpleform.html')
+
+@app.route('/select')
+def select():
+    cursor = mysql.connection.cursor()
+
+    select = cursor.execute("SELECT * from pruebas")
+
+    if select > 0:
+        #se utiliza fetchall  dado que no esperamos demasiados archivos
+
+        userDetails = cursor.fetchall()
+
+        return render_template('select_table.html',userDetails = userDetails)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
