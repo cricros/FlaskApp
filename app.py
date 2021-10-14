b3ae0566b18552587575d690bfb08eb6f8f23e66
Cristian <vce11987@innovaccion.mx>
2021-10-13 19:34:20 -0500

    segundo commit

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
-
+    return 'Hola amigos de Geeky Theory!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
--- pruebasbd.py
+++ pruebasbd.py
@@ -17,16 +17,18 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'pruebas_python'
+app.config['MYSQL_DB'] = 'componentes'
 
 mysql = MySQL(app)
+
+
 
 @app.route("/")
 def index():
     return render_template("index.html") #cambiar el formato de index.hmtl
 
 
-
+'''
 @app.route('/comp1', methods=['GET','POST'])
 def comp1():
 
@@ -51,7 +53,7 @@ def comp1():
 def select():
     cursor = mysql.connection.cursor()
 
-    select = cursor.execute("SELECT * from pruebas")
+    select = cursor.execute("SELECT * from graficas where MarcaG = 'AMD';")
 
     if select > 0:
         #se utiliza fetchall  dado que no esperamos demasiados archivos
@@ -61,6 +63,6 @@ def select():
         return render_template('select_table.html',userDetails = userDetails)
 
 
-
+'''
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
